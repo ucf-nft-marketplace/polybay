920476d691e68067bd5e4f36b644bd0e3096bc8f
Angelm116 <44384080+Angelm116@users.noreply.github.com>
2021-11-05 12:23:45 -0400

    Added market smart contract and deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,25 @@
-require("@nomiclabs/hardhat-waffle");
-
-// Setting up for Polygon
-const projectID = "e2d0987d41a546a38b1f1288985bf5d1";
+require("@nomiclabs/hardhat-waffle")
+const fs = require('fs')
+const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789"
 
 module.exports = {
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      chainID: 1337
+      chainId: 1337
     },
     mumbai: {
-      url: `https://polygon-mumbai.infura.io/v3/${projectID}`,
-      accounts: []
-    },
-    mainnet: {
-      url: `https://polygon-mumbai.infura.io/v3/${projectID}`,
-      accounts: []
+      url: "https://rpc-mumbai.matic.today",
+      accounts: [privateKey]
     }
   },
-  solidity: "0.8.4",
-};
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
