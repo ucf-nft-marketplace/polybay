9694ad186be45ee7b1eee4c0bad5a68d7cd76e2d
bruno ebihara <bruno.ebihara@gmail.com>
2022-01-26 16:39:06 -0500

    Added css to creator-dashboard page

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import Navbar from '../components/Navigation'
 
 function MyApp({ Component, pageProps }) {
     return (
-        <div className="bg-black h-full min-h-screen text-white">
+        <div className="bg-black h-full min-h-screen text-white font-body">
             <Navbar />
             <Component {...pageProps} />
         </div>
