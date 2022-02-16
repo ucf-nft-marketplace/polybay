71a27b14320720d54d10fdc7bf7477d11134cee2
bruno ebihara <bruno.ebihara@gmail.com>
2022-02-16 15:19:39 -0500

    implemented dialog box to appear when clicked on nft 'details'

--- pages/creator-dashboard/index.js
+++ pages/creator-dashboard/index.js
@@ -1,7 +1,7 @@
 import { ethers } from 'ethers'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import Web3Modal from "web3modal"
+import { Dialog } from '@headlessui/react'
 
 import {
   nftmarketaddress, nftaddress
@@ -22,9 +22,31 @@ export default function CreatorDashboard() {
   // creator - sold nfts
   const [sold, setSold] = useState([])
   const [loadingState, setLoadingState] = useState('not-loaded')
+
+  const [isShown, setIsShown] = useState(false)
+  const [isKey, setKey] = useState(null)
+
   useEffect(() => {
     loadNFTs()
   }, [])
+
+  function onEnter(key) {
+    setIsShown(true)
+    setKey(key)
+  }
+
+  function onLeave() {
+    setIsShown(false)
+    setKey(null)
+  }
+
+  // checks current key with selected key, so that the correct nft is displayed 
+  function checkKey(isShown, key, i) {
+    if ((isShown) && (key === i) ){
+      return true
+    }
+    return false
+  }
 
   async function loadNFTs() {
     const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
@@ -39,6 +61,7 @@ export default function CreatorDashboard() {
       let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
       let item = {
         name: meta.data.name,
+        description: meta.data.description,
         price,
         tokenId: i.tokenId.toNumber(),
         seller: i.seller,
@@ -62,6 +85,7 @@ export default function CreatorDashboard() {
           <div className="nft-grid">
           {
             nfts.map((nft, i) => (
+              <>           
               <div key={i} className="nft-item group">
                 <img src={nft.image} className="h-72 w-full" />
                 <div className="flex h-20 p-3.5 border rounded-xl rounded-t-none">
@@ -70,12 +94,40 @@ export default function CreatorDashboard() {
                     <p className="text-xl font-semibold">{nft.price} ETH</p>
                   </div>
                   <div className="w-1/5 self-end mr-2">
-                    <button className="text-sm hidden focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75 group-hover:block">
+                    <button onClick={() => onEnter(i)} className="text-sm hidden focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75 group-hover:block">
                       details
                     </button>
                   </div>
                 </div>
               </div>
+              <Dialog className="fixed inset-0 z-10 overflow-y-auto" open={checkKey(isShown, isKey, i)} onClose={() => onLeave()} >
+                  <div className="min-h-screen px-4 text-center">
+                    {/* handle clicking outside of dialog */}
+                    <Dialog.Overlay className="fixed inset-0" />
+                    {/* centers dialog to screen */}
+                    <span className="inline-block h-screen align-middle" aria-hidden="true">
+                      &#8203;
+                    </span>
+                      <div className="inline-block w-full max-w-md p-6 my-8 border overflow-hidden text-left align-middle transform bg-black rounded-2xl">
+                        <Dialog.Title className="text-lg font-medium leading-6 text-white">
+                          {nft.name}
+                        </Dialog.Title>
+                        <div className="mt-2 text-white">
+                          <img src={nft.image}/>
+                          <div className="mt-2 text-sm">
+                            <p> {nft.description} </p>
+                          </div>
+                          <div className="mt-2 text-sm grid grid-cols-2">
+                              <p className="font-light"> contract address </p>
+                              <a className="font-medium text-purple-300 text-right" href="https://mumbai.polygonscan.com/address/0xc9ad4e81bbcff8b642ed55adb5da7f383442e351">0xc9ad4e81...3442e351</a>
+                              <p className="font-light"> token ID </p>
+                              <p className="font-medium text-gray-400 text-right"> {nft.tokenId} </p>
+                          </div>
+                        </div>
+                      </div>
+                  </div>
+                </Dialog>
+              </>
             ))
           }
         </div>
@@ -88,6 +140,7 @@ export default function CreatorDashboard() {
               <div className="nft-grid">
                 {
                   sold.map((nft, i) => (
+                    <>
                     <div key={i} className="nft-item group">
                       <img src={nft.image} className="h-72 w-full" />
                       <div className="flex h-20 p-3.5 border rounded-xl rounded-t-none">
@@ -96,12 +149,40 @@ export default function CreatorDashboard() {
                           <p className="text-xl font-semibold">{nft.price} ETH</p>
                         </div>
                         <div className="w-1/5 self-end mr-2">
-                          <button className="text-sm hidden focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75 group-hover:block">
+                          <button onClick={() => onEnter(i)} className="text-sm hidden focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75 group-hover:block">
                             details
                           </button>
                         </div>
                       </div>
                     </div>
+                    <Dialog className="fixed inset-0 z-10 overflow-y-auto" open={checkKey(isShown, isKey, i)} onClose={() => onLeave()} >
+                      <div className="min-h-screen px-4 text-center">
+                        {/* handle clicking outside of dialog */}
+                        <Dialog.Overlay className="fixed inset-0" />
+                        {/* centers dialog to screen */}
+                        <span className="inline-block h-screen align-middle" aria-hidden="true">
+                          &#8203;
+                        </span>
+                          <div className="inline-block w-full max-w-md p-6 my-8 border overflow-hidden text-left align-middle transform bg-black rounded-2xl">
+                            <Dialog.Title className="text-lg font-medium leading-6 text-white">
+                              {nft.name}
+                            </Dialog.Title>
+                            <div className="mt-2 text-white">
+                              <img src={nft.image}/>
+                              <div className="mt-2 text-sm">
+                                <p> {nft.description} </p>
+                              </div>
+                              <div className="mt-2 text-sm grid grid-cols-2">
+                                  <p className="font-light"> contract address </p>
+                                  <a className="font-medium text-purple-300 text-right" href="https://mumbai.polygonscan.com/address/0xc9ad4e81bbcff8b642ed55adb5da7f383442e351">0xc9ad4e81...3442e351</a>
+                                  <p className="font-light"> token ID </p>
+                                  <p className="font-medium text-gray-400 text-right"> {nft.tokenId} </p>
+                              </div>
+                            </div>
+                          </div>
+                      </div>
+                    </Dialog>
+                    </>
                   ))
                 }
               </div>
